669d8a5eb88597e2cd857cd12f2139fe6ca780b2
alpesh patil <alpeshpatilalpesh91@gmail.com>
2023-03-07 18:37:28 +0530

    3/7/2023

--- init.py
+++ init.py
@@ -8,9 +8,21 @@ cursor = connection.cursor()
 
 app = Flask(__name__)
 
+@app.route('/showTables',methods=['GET','POST'])
+def showTables():
+    dict={}
+    query=("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+    cursor.execute(query)
+    result=cursor.fetchall()
+    i=1
+    for row in result:
+        dict[i]=row
+        i=i+1
+    return dict
+    
 @app.route('/getData',methods=['GET',"POST"])
 def getData():
-    _req=request.form
+    _req=request.json
     tableName=_req['tableName']
     cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_name = %s", (tableName,))
 
@@ -24,8 +36,29 @@ def getData():
     for key in column_dict:
         value =_req[key].format(key)
         column_dict[key] = value
-    result=reportBuilder.dataOfTable(tableName,column_dict)
-    return result
+        
+    var=str(tableName)
+    
+# Build the SELECT statement based on the dictionary values
+    select_stmt = "SELECT "
+    column_names = [key for key in column_dict.keys() if column_dict[key]]
+    select_stmt += ", ".join(column_names)
+    select_stmt += " FROM " + var
+    
+    list=[]
+    
+    # Execute the SELECT statement and fetch the results
+    cursor.execute(select_stmt)
+    selected_data = cursor.fetchall()
+    for row in selected_data:
+        ans={}
+        result_dict = {}
+        for i in range(len(column_names)):
+            result_dict[column_names[i]] = row[i]
+        ans=result_dict
+        list.append(ans)
+    # result=reportBuilder.dataOfTable(tableName,column_dict)
+    return jsonify(list)
 
 @app.route('/viewDataByMultipleAngles',methods=['GET',"POST"])
 def viewDataByMultipleAngles():
@@ -44,9 +77,11 @@ def viewDataByMultipleAngles():
         value =_req[key].format(key)
         column_dict[key] = value
     sum1=_req['sum1']
-    sum2=_req['sum2']
+    date=_req['date']
+    start_date=_req['sdate']
+    end_date=_req['edate']
     groupBy=_req['group_by']
-    result=reportBuilder.viewData(tableName,column_dict,groupBy,sum1,sum2)
+    result=reportBuilder.viewData(tableName,column_dict,groupBy,sum1,date,start_date,end_date)
     return result
 
 if __name__ == '__main__':
--- reportBuilder.py
+++ reportBuilder.py
@@ -4,32 +4,24 @@ import requests
 connection = psycopg2.connect(user="postgres",password="root",host="localhost",port="5433",database="postgres")
 cursor = connection.cursor()
 
-def dataOfTable(tableName,column_dict):
-    result=[]
-    var=str(tableName)
-# Build the SELECT statement based on the dictionary values
-    select_stmt = "SELECT "
-    column_names = [key for key in column_dict.keys() if column_dict[key]]
-    select_stmt += ", ".join(column_names)
-    select_stmt += " FROM " + var
-
-    # Execute the SELECT statement and fetch the results
-    cursor.execute(select_stmt)
-    results = cursor.fetchall()
-    for row in results:
-        result.append(row)
-    return result
-
-def viewData(tableName,column_dict,groupBy,sum1,sum2):
+def viewData(tableName,column_dict,groupBy,sum1,date,start_date,end_date):
     result=[]
     var=str(tableName)
 # Build the SELECT statement based on the dictionary values
 # Execute the query and print the results
-    
     group_by_column =str(groupBy)
     var1=str(sum1)
-# Build the GROUP BY query
-    if group_by_column in ['customer_name','price', 'region', 'date']:
+# betwenn any date query
+    if start_date or end_date:
+        query = "SELECT "
+        column_names = [key for key in column_dict.keys() if column_dict[key]]
+        query += ", ".join(column_names)
+        query += " FROM " + var
+        query+=" WHERE " + column_dict['sale_date']
+        query+=" BETWEEN  " + start_date 
+        query+=" AND " + end_date
+        # Build the GROUP BY query
+    elif group_by_column in ['quantity','customer_name','price', 'region', 'date']:
         query = ("SELECT {group_by_column}, SUM({var1}) FROM {var} GROUP BY {group_by_column}".format(group_by_column=group_by_column,var1=var1,var=var))
     else:
         print("Invalid column name.")
